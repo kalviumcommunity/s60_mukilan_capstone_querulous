ba63023179039a54cbf9aa373e04e1a15f6f7f8f
MUKILAN019 <mukilan.p@kalvium.community>
2024-07-30 18:40:56 +0530

    added new components file and added authentication

--- Front_End/src/App.jsx
+++ Front_End/src/App.jsx
@@ -3,17 +3,7 @@ import Splash_Page from './Components/Splash_Page';
 import {Routes,Route} from "react-router-dom";
 import LandingPage from './Components/LandingPage';
 import SignUp from './Components/SignUp';
-import OTP from './Components/OTP';
-import HomePage from './Components/Home';
-import Profile from './Components/Profile';
 import ChoicePage from './Components/Choice';
-import ProfileEdit from './Components/ProfileEdit';
-import ChatBox from './Components/Chat';
-import PostPage from './Components/Post';
-import PostAdd from './Components/PostAdd';
-import Edit from './Components/Edit';
-import ChangePass from './Components/ChangePass';
-import NewsApp from './Components/NewsApp';
 
 function App() {
 
@@ -24,17 +14,8 @@ function App() {
            <Route path="/" element={<Splash_Page/>}/>
            <Route path="/landingPage" element={<LandingPage/>}/>
            <Route path="/SignUp" element={<SignUp/>}/>
-           <Route path="/OTP" element={<OTP/>}/>
-           <Route path="/home" element={<HomePage/>}/>
-           <Route path="/profile" element={<Profile/>}/>
            <Route path="/choice" element={<ChoicePage/>}/>
-           <Route path="/edit" element={<ProfileEdit/>}/>
-           <Route path="/chat" element={<ChatBox/>}/>
-           <Route path="/post" element={<PostPage/>}/>
-           <Route path="/add" element={<PostAdd/>}/>
-           <Route path="/editPost/:id" element={<Edit/>}/>
-           <Route path='/rec' element={<ChangePass/>}/>
-           <Route path='/news' element={<NewsApp/>}/>
+           
        </Routes>
    
     </>
